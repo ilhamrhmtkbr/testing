bb9f86db30d4884894f26985c5c225eb2b0d9f21
Ilham Rahmat Akbar <ilhamrhmtkbr@gmail.com>
2025-10-30 06:24:02 +0700

    oke10

--- backend-api-instructor/tests/TestCase.php
+++ backend-api-instructor/tests/TestCase.php
@@ -8,7 +8,7 @@ use Tests\utils\Repository;
 
 abstract class TestCase extends BaseTestCase
 {
-    public string $token;
+    public ?string $token = null;
     public string $url;
 
     protected function setUp(): void
@@ -17,13 +17,15 @@ abstract class TestCase extends BaseTestCase
         Repository::insertInstructor();
         Repository::insertStudent();
         $this->url = config('api.version');
-        $res = Http::post('http://backend-api-user:8000/'. env('USER_API_VERSION') . '/auth/login', [
-            'username' => Repository::INSTRUCTOR_USERNAME,
-            'password' => Repository::INSTRUCTOR_PASSWORD
-        ]);
-        $cookies = $res->header('Set-Cookie');
-        preg_match('/access_token=([^;]+)/', $cookies, $matches);
-        $this->token = $matches[1] ?? null;
+        if ($this->token === null) {
+            $res = Http::post('http://backend-api-user:8000/'. env('USER_API_VERSION') . '/auth/login', [
+                'username' => Repository::INSTRUCTOR_USERNAME,
+                'password' => Repository::INSTRUCTOR_PASSWORD
+            ]);
+            $cookies = $res->header('Set-Cookie');
+            preg_match('/access_token=([^;]+)/', $cookies, $matches);
+            $this->token = $matches[1] ?? null;
+        }
     }
 
     protected function tearDown(): void
