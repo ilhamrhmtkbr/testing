a45ccf3176e381f01ff4c578d2631cc696442d94
Ilham Rahmat Akbar <ilhamrhmtkbr@gmail.com>
2025-10-30 07:17:35 +0700

    oke13

--- backend-api-instructor/tests/TestCase.php
+++ backend-api-instructor/tests/TestCase.php
@@ -8,7 +8,7 @@ use Tests\utils\Repository;
 
 abstract class TestCase extends BaseTestCase
 {
-    public ?string $token = null;
+    protected static ?string $token = null;
     public string $url;
 
     protected function setUp(): void
@@ -17,14 +17,18 @@ abstract class TestCase extends BaseTestCase
         Repository::insertInstructor();
         Repository::insertStudent();
         $this->url = config('api.version');
-        if ($this->token === null) {
+        if (self::$token === null) {
             $res = Http::post('http://backend-api-user:8000/'. env('USER_API_VERSION') . '/auth/login', [
                 'username' => Repository::INSTRUCTOR_USERNAME,
                 'password' => Repository::INSTRUCTOR_PASSWORD
             ]);
             $cookies = $res->header('Set-Cookie');
             preg_match('/access_token=([^;]+)/', $cookies, $matches);
-            $this->token = $matches[1] ?? null;
+            self::$token = $matches[1] ?? null;
+
+            if (self::$token === null) {
+                throw new \Exception('Failed to get access token from user API. Response: ' . $res->body());
+            }
         }
     }
 
